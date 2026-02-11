12e4c058c7bac802efd6cf61012973a0877e9af9
Swagatika Beura <swagatikab25@gmail.com>
2026-02-11 12:07:22 +0000

    added a new file

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,10 @@
+
+let name="swagatika"
+let age=16
+let isloggedIn= true
+
+console.log(typeof "swagatika")
+console.log(typeof "swagatika")
+console.log(typeof "swagatika")
+console.log(typeof "swagatika")
+
---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountid=133434
+let accountEmail="swe@gmail.com"
+var password=98587389
+let account
+
+//console.log(password)
+console.log([accountid,accountEmail,password,account])
